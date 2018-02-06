ed81330711494fc4782a76db77cca99314f88483
cryptariat <35814914+cryptariat@users.noreply.github.com>
2018-02-06 12:25:27 -0500

    Daemon & money supply

--- q/supply/index.php
+++ q/supply/index.php
@@ -3,7 +3,7 @@
 
 $data_string = '{"jsonrpc":"2.0","id":"test","method":"getlastblockheader","params":" "}';
 
-$ch = curl_init('http://explorer.mining.blue/domain/json_rpc');
+$ch = curl_init('http://xlv.world:12023/json_rpc');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -30,7 +30,7 @@ curl_close($ch);
 $data_string2 = '{"jsonrpc":"2.0","id":"test","method":"f_block_json","params":{"hash":"'.$hash.'"}}';
 
 
-$ch2 = curl_init('http://explorer.mining.blue/domain/json_rpc');
+$ch2 = curl_init('http://xlv.world:12023/json_rpc');
 curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch2, CURLOPT_POSTFIELDS, $data_string2);
 curl_setopt($ch2, CURLOPT_RETURNTRANSFER, true);
@@ -48,7 +48,7 @@ $blockData = json_decode($block, TRUE);
 
 $supply = $blockData['result']['block']['alreadyGeneratedCoins'];
 
-$supply  = number_format($supply / 1000000000000, 12, ".", "");
+$supply  = number_format($supply / 18446744, 12, ".", "");
 
 print_r($supply);
 
