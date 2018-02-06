eb0624ec26ec1d9bf1f15c958b3edd05f7c7dad9
cryptariat <35814914+cryptariat@users.noreply.github.com>
2018-02-06 12:21:25 -0500

    Daemon

--- q/height/index.php
+++ q/height/index.php
@@ -2,7 +2,7 @@
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL, 'http://explorer.mining.blue/domain/getinfo');
+curl_setopt($ch, CURLOPT_URL, 'http://xlv.world:12023/getinfo');
 $result = curl_exec($ch);
 $obj = json_decode($result, TRUE);
 //print_r($obj);
