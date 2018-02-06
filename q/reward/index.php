889579de13cd09d179b50403a0d9c9efd262c80d
cryptariat <35814914+cryptariat@users.noreply.github.com>
2018-02-06 12:23:52 -0500

    Daemon & money supply

--- q/reward/index.php
+++ q/reward/index.php
@@ -1,6 +1,6 @@
 <?php 
 $data_string = '{"jsonrpc":"2.0","id":"test","method":"getlastblockheader","params":" "}';
-$ch = curl_init('http://explorer.mining.blue/domain/json_rpc');
+$ch = curl_init('http://xlv.world:12023/json_rpc');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -11,7 +11,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 $result = curl_exec($ch);
 $responseData = json_decode($result, TRUE);
 $rewardRaw = $responseData['result']['block_header']['reward'];
-$reward  = number_format($rewardRaw / 1000000000000, 12, ".", "");
+$reward  = number_format($rewardRaw / 18446744, 12, ".", "");
 print_r($reward);
 curl_close($ch);
 ?>
